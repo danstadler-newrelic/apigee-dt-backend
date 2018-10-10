ef74dcdf5531662d31b82cc699327891fbd91b32
Dan Stadler <dstadler@newrelic.com>
2018-10-10 18:51:10 +0000

    modified to work with Apigee, and to use a simple page to monitor inbound calls via Websockets

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 var express = require('express')
-var ws = require('./ws')
+
+var WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({port: 40510})
+
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    console.log('received: %s', message)
+  })
+  ws.send(`${new Date()}`)
+})
+
+// Broadcast to all connected clients
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    //console.log(data);
+    var stringed = JSON.stringify(data)
+    client.send(stringed);
+  });
+};
+
+// Static test var
+var test_message = {
+    'value1': 'ABC',
+    'value2': 'DEF'
+};
 
 var app = express()
 
+// you can open this page, and then open the browser's console, 
+// to monitor inbound calls to /apigee-ingress-point
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
+})
+
+// this function represents the ingress point for Apigee.
+// it broadcasts to WebSocket clients (like in '/') that a call came in, 
+// and it then sends the same test message back to the upstream caller.
+app.get('/apigee-ingress-point', function (req, res) {
+  // broadcast to clients
+  wss.broadcast(test_message);
+  // respond to upstream callers
+  res.send(JSON.stringify(test_message))
 })
 
 app.listen(3000, function () {
