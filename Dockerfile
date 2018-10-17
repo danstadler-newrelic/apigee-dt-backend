4b06f15181efcbca6547813efcfc7897b1849a1c
Dan Stadler <dstadler@newrelic.com>
2018-10-17 23:51:08 +0000

    remove websocket and parameterize env vars into dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM node:8
+ARG newrelic_appname=your-app-name
+ARG newrelic_license=your-license-key
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 3000 40510
-ENV NEW_RELIC_APP_NAME=your-app-name-here \
-    NEW_RELIC_LICENSE_KEY=your-license-key-here \
-    DOMAIN_AND_PORT=your-domain-and-port-here
+ENV NEW_RELIC_APP_NAME=$newrelic_appname \
+    NEW_RELIC_LICENSE_KEY=$newrelic_license
 CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -1,42 +1,16 @@
 require('newrelic');
 var express = require('express')
-
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
-
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
-  ws.send(`${new Date()}`)
-})
-
-// Broadcast to all connected clients
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    //console.log(data);
-    var stringed = JSON.stringify(data)
-    client.send(stringed);
-  });
-};
-
 var app = express()
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-// you can open this page, and then open the browser's console, 
-// to monitor inbound calls to /apigee-ingress-point
 app.get('/', function (req, res) {
-    res.render('ws', { domainAndPort: process.env.DOMAIN_AND_PORT })
+    res.render('ws')
 })
 
 // this function represents the ingress point for Apigee.
-// it broadcasts to WebSocket clients (like in '/') that a call came in, 
-// and it then sends the same test message back to the upstream caller.
+// it sends a test message back to the upstream caller.
 app.get('/apigee-ingress-point', function (req, res) {
-
-  // broadcast to clients
-  wss.broadcast(test_message);
 
   // respond to upstream callers
   var test_message = {
